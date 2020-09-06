e75ed72a9381b7a028e01436fad083cf74cbdd9e
David <fasinadavid@gmail.com>
2020-09-05 19:12:38 -0500

    added char shift

--- encode.js
+++ encode.js
@@ -1,5 +1,23 @@
-function encoder () {
-    var text = document.getElementById("message").value;
-    document.getElementById("demo").innerHTML = text;
+
+
+
+
+function incrChar(ch, i) {
+    return String.fromCharCode(ch[i] + 1);            
 }
 
+function encoder () {
+
+    var text = document.getElementById("message").value;
+    var text = [...text];
+    var newText = []; 
+    //create loop w/ incr
+    for (let i = 0; i < text.length; i++) {
+        newText[i] = String.fromCharCode(text[i].charCodeAt() + 1);
+    }
+    newText = newText.join('');
+
+    document.getElementById("demo").innerHTML = text;
+    document.getElementById("message").innerHTML = Array.from(text)
+
+}
